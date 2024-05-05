564672aeb38bde3ec5729310d3090d8a420cda49
masamallow <77992722+masamallow@users.noreply.github.com>
2024-05-05 17:16:03 +0900

    feat: Add Search in Private Browsing command (#1)

---
+++ commands/search-in-private-browsing.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Auto generated script by Raycast.
+# And reference source:
+# script-commands/commands/browsing/to-private-browsing.sh at master raycast/script-commands GitHub
+# https://github.com/raycast/script-commands/blob/master/commands/browsing/to-private-browsing.sh
+
+# Note: Browser app required (Google Chrome or Brave Browser)
+# Install via official website.
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Search in Private Browsing
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName Browser
+# @raycast.icon 🥽
+# @raycast.argument1 { "type": "text", "placeholder": "Query", "percentEncoded": false }
+
+# Documentation:
+# @raycast.description Search for the entered words or phrases in the private browsing of the browser specified in the script.
+# @raycast.author masamallow
+# @raycast.authorURL https://github.com/masamallow
+
+############################################
+## Set your browser app                   ##
+## "Google Chrome"                        ##
+## "Brave Browser"                        ##
+############################################
+
+browser="Google Chrome"
+
+URL="https://www.google.com/search?q=$1"
+open -a "$browser" -n --args --incognito "$URL"
